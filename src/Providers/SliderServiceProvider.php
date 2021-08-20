a988cbec23feea4c275727e65a3424341fb2c257
Sahadat_hossain <mssunny202146@gmail.com>
2021-08-20 22:00:16 +0600

    first commit this is not in working condition

---
+++ src/Providers/SliderServiceProvider.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace Ponditcommerce\Slider\Providers;
+use Illuminate\Support\ServiceProvider;
+
+
+class SliderServiceProvider extends ServiceProvider{
+
+    public function register(){
+
+        
+
+    }
+
+    public function boot(){
+
+    }
+
+}
